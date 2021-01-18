47173834e6ffdc7e46ea4b742612701dbf12541e
TomerNabatian <TomerNabatian@users.noreply.github.com>
2021-01-18 20:44:42 +0200

    10 question

---
+++ 10.js
@@ -0,0 +1,11 @@
+"use strict"
+let x,y,triangle;
+triangle="";
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    triangle=triangle+("*");        
+      }
+ console.log(triangle);   
+}
