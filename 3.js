e4085e7fc5be5cd845f72c81d8a29fa1bd9ff9cb
TomerNabatian <TomerNabatian@users.noreply.github.com>
2021-01-18 19:14:41 +0200

    third answer

---
+++ 3.js
@@ -0,0 +1,19 @@
+"use strict"
+let num1 =prompt("type first number");
+let num2=prompt("type second number");
+let num3=prompt("type third number");
+if (num1>num2 && num1>num3)
+        if (num2>num3)
+        console.log(num1,num2,num3);
+        else
+        console.log(num1,num3,num2);
+else if (num2>num1 && num2 >num3)
+        if (num1>num3)
+        console.log(num2,num1,num3);
+        else
+        console.log(num2,num3,num1);
+else if (num3>num1 && num3>num2)
+        if (num1>num2)
+        console.log(num3,num1,num2);
+        else
+        console.log(num3,num2,num1);   
