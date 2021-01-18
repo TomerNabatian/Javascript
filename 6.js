72c4cfe2f79f9684358ae2abe9568f64ae6914db
TomerNabatian <TomerNabatian@users.noreply.github.com>
2021-01-18 19:53:12 +0200

    6th answer

---
+++ 6.js
@@ -0,0 +1,19 @@
+"use strict"
+let david=80;
+let vinoth=77;
+let divya=88;
+let ishitha=95;
+let thomas=68;
+let avg= (david+vinoth+divya+ishitha+thomas)/5
+
+
+if (avg < 60)
+    console.log("Grade : F");      
+else if (avg < 70) 
+          console.log("Grade : D"); 
+else if (avg < 80) 
+          console.log("Grade : C"); 
+else if (avg < 90) 
+          console.log("Grade : B"); 
+else if (avg < 100) 
+          console.log("Grade : A"); 
