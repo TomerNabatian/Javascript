8783fd79a5faa33f78aaaadb6a6ad720078b9dd9
TomerNabatian <TomerNabatian@users.noreply.github.com>
2021-01-18 19:24:38 +0200

    4th question

---
+++ 4.js
@@ -0,0 +1,28 @@
+"use strict"
+let num1,num2,num3,num4,num5
+num1=prompt("Type the first number");
+num2=prompt("Type the second number");
+num3=prompt("Type the third number");
+num4=prompt("Type the fourth number");
+num5=prompt("Type the fifth number");
+
+if (num1>num2 && num1>num3 && num1>num4 && num1>num5)
+{
+    console.log(num1);
+}
+else if (num2>num1 && num2>num3 && num2>num4 && num2>num5)
+{
+    console.log(num2);
+}
+else if (num3>num1 && num3>num2 && num3>num4 && num3>num5)
+{
+    console.log(num3);
+}
+else if (num4>num1 && num4>num3 && num4>num2 && num4>num5)
+{
+    console.log(d);
+}
+else
+{
+    console.log(num5);
+}
