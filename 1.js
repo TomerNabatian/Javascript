de7f2da57b798fb8a0a7b5cf98aef9df85731b86
TomerNabatian <TomerNabatian@users.noreply.github.com>
2021-01-18 18:50:36 +0200

    1+2

---
+++ 1.js
@@ -0,0 +1,9 @@
+"use strict"
+let num1,num2;
+num1=prompt("type your first number.");
+num2=prompt("type your second number.");
+ if(num1>num2)
+     console.log(num1);
+     else
+     console.log(num2);
+ 
---
+++ 2.js
@@ -0,0 +1,17 @@
+"use strict"
+let num1,num2,num3;
+num1=prompt("type the first number");
+num2=prompt("type the second number");
+num3=prompt("type the third number");
+if(num1>0)
+    alert("The sign is +");
+else
+    alert("The sign is -");
+if(num2>0)
+    alert ("The sign is +");
+else
+    alert("The sign is -");
+if(num3>0)
+    alert("The sign is +");
+else
+    alert("The sign is -");
